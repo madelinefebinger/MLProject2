88d38d19804dcf8ac79ee6b33b67644cbd955903
Kevin Ackerman <ackermk3@tcnj.edu>
2019-04-04 16:59:33 -0400

    Fixed generator

--- generator.py
+++ generator.py
@@ -7,8 +7,8 @@ def generate(num, attr1_vals,attr2_vals,attr3_vals,attr4_vals,attr5_vals,attr6_v
 	training_examples = []
 	i = 0	
 	while i < num:
-		training_examples.append([attr1_vals[random.randint(0,4)], attr2_vals[random.randint(0,3)],attr3_vals[random.randint(0,3)],
-		attr4_vals[random.randint(0,3)], attr5_vals[random.randint(0,3)], attr6_vals[random.randint(0,3)], 'classify'])
+		training_examples.append([attr1_vals[random.randint(0,2)], attr2_vals[random.randint(0,1)],attr3_vals[random.randint(0,1)],
+		attr4_vals[random.randint(0,1)], attr5_vals[random.randint(0,1)], attr6_vals[random.randint(0,1)], 'classify'])
 		i+=1
 
 	return training_examples
@@ -20,9 +20,6 @@ def classify(training_examples, target):
 		for k in range(0,6):
 			if target[k] == '?':
 				passing +=1
-			elif i[k] == '0':
-				i[6] = 'No'
-				break
 			elif target[k] == i[k]:
 				passing +=1
 		if passing == 6:
@@ -35,12 +32,12 @@ def classify(training_examples, target):
 if __name__ == '__main__':
 
 	target = ['Sunny', 'Warm', '?', '?', '?', '?']
-	sky = ['0', '?', 'Sunny', 'Rainy', 'Cloudy']
-	temp = ['0', '?', 'Warm', 'Cold']
-	humidity = ['0', '?', 'High', 'Normal']
-	wind = ['0', '?', 'Strong', 'Weak']
-	water = ['0', '?', 'Sunny', 'Rainy']
-	forecast = ['0', '?', 'Same', 'Change']
+	sky = ['Sunny', 'Rainy', 'Cloudy']
+	temp = ['Warm', 'Cold']
+	humidity = ['High', 'Normal']
+	wind = ['Strong', 'Weak']
+	water = ['Sunny', 'Rainy']
+	forecast = ['Same', 'Change']
 
 	random_examples = generate(100, sky, temp, humidity, wind, water, forecast)
 	classified = classify(random_examples, target)
